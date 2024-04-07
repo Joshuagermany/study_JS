c4100ad33bb5c427942dd448ec3ef622c1880db3
Joshuagermany <101688691+Joshuagermany@users.noreply.github.com>
2024-04-07 00:55:32 +0000

    second commit

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const app = express()
 const port = 5000
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://joshuagermany:joshua2003@training.ygcsn6c.mongodb.net/?retryWrites=true&w=majority&appName=training')
+const config = require('./config/key')
+const { User } = require('./models/User')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB is Connected...'))
 .catch((err) => console.log(err))
 
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    //get the info for register from USER, and post it to DB
+    const user = new User(req.body)
+    user.save((err, userinfo) => {
+        if(err) return res.json({ success: false, err })
+        return res.status(200).json({ success: true })
+    })
 })
 
 app.listen(port, () => {
